0d97ef80e8a56bdb2029613df30debaf8f287579
Livinsky Stas <livinskystas@host-192-168-88-242.sohonet.ua>
2023-09-30 21:31:27 +0300

    fix the create SQL script

--- src/main/resources/db.migration.dating_admin_tracker/V1__Create_admin_panel_sales_table.sql.sql
+++ src/main/resources/db.migration.dating_admin_tracker/V1__Create_admin_panel_sales_table.sql.sql
@@ -1,4 +1,4 @@
-CREATE TABLE admin_panel_sales (
+CREATE TABLE IF NOT EXISTS admin_panel_sales (
 id BIGINT AUTO_INCREMENT PRIMARY KEY,
 application_date DATE NOT NULL,
 client_contact VARCHAR(255) NOT NULL,
@@ -8,3 +8,20 @@ price DOUBLE NOT NULL,
 notes TEXT,
 document_url VARCHAR(255)
 );
+DROP PROCEDURE IF EXISTS alterNoFailure;;
+create procedure alterNoFailure ()
+begin
+    declare continue handler for 1060 begin end;
+    ALTER TABLE  admin_panel_sales
+        ADD COLUMN client_telegram VARCHAR(255) NOT NULL,ALGORITHM=INPLACE, LOCK=NONE;
+end;;
+call alterNoFailure();;
+
+DROP PROCEDURE IF EXISTS alterNoFailure;;
+create procedure alterNoFailure ()
+begin
+    declare continue handler for 1060 begin end;
+    ALTER TABLE  admin_panel_sales
+        ADD COLUMN client_telegram VARCHAR(255) NOT NULL,ALGORITHM=INPLACE, LOCK=NONE;
+end;;
+call alterNoFailure();;
